1e9341e5bc0e3d4f66b03668c0df9f0949093164
Tunahan Balcı <158046109+Tunahan23233@users.noreply.github.com>
2025-11-12 18:31:33 +0300

    Add files via upload

---
+++ classic_js_parcasi.js
@@ -0,0 +1,18 @@
+﻿        // Sayfa içi bağlantılar için smooth scroll (isteğe bağlı)
+    const links = document.querySelectorAll('nav a');
+links.forEach(link => {
+    link.addEventListener('click', (e) => {
+        const href = link.getAttribute('href');
+        if (href.startsWith('#')) {
+            e.preventDefault();
+            const id = href.substring(1);
+            const section = document.getElementById(id);
+            if (section) {
+                window.scrollTo({
+                    top: section.offsetTop - 80,
+                    behavior: 'smooth'
+                });
+            }
+        }
+    })
+})
